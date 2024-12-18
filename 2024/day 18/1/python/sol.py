74f9f96fe332f43f5071d4559ce38670cf1494b3
oPeras <hugo.s.pereira@tecnico.ulisboa.pt>
2024-12-18 14:46:22 +0000

    pretty ez

---
+++ 2024/day 18/1/python/sol.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+map2d = [["."] * 71 for _ in range(71)]
+
+
+coords = []
+bts = 0
+with open("../../input.txt") as file:
+    for line in file:
+        if bts >= 1024:
+            break
+        bts += 1
+        x, y = line.strip().split(",")
+        coords.append((int(x), int(y)))
+        map2d[int(y)][int(x)] = "X"
+
+
+def bfs(start, goal):
+    queue = deque()
+    queue.append((start, [start]))
+    visited = set()
+
+    while queue:
+        (x, y), path = queue.popleft()
+
+        if (x, y) == goal:
+            return path
+
+        if (x, y) in visited:
+            continue
+
+        visited.add((x, y))
+        
+        for dx, dy in [(-1,0),(1,0),(0,-1),(0,1)]:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx <= 70 and 0 <= ny <= 70 and map2d[ny][nx] != "X":
+                queue.append(((nx, ny), path + [(nx, ny)]))
+    return None
+
+path = bfs((0, 0), (70, 70))
+
+print(len(path) - 1)
+
